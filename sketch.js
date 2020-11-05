387c819f0319b93db5c5b5cda518efbd404b6ecd
PonnamSiddhartha <73280330+PonnamSiddhartha@users.noreply.github.com>
2020-11-05 16:17:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,56 @@
-var car,wall;
+var bullet,wall,thickness;
 var speed,weight;
 function setup() {
   createCanvas(1600,400);
-  wall=createSprite(1500,200,30,height/2);
+  wall=createSprite(1200,200,thickness,height/2);
  
-  car=createSprite(50,200,50,50)
- speed=random(55,90);
- weight=random(400,1500);
+  bullet=createSprite(50,200,50,50);
+  bullet.shapeColor="white"
+ speed=random(223,321);
+ weight=random(30,52);
+ thickness=random(22,88);
 }
 
 function draw() {
   background(255,255,255);  
-  car.velocityX=speed;
+  bullet.velocityX=speed;
   wall.shapeColor="80,80,80";
-  if(wall.x-car.x<(car.width+wall.width)/2)
+  if (hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+    var damage=0.5*weight*speed/(thickness*thickness*thickness);
+  }
+  if(damage>10){
+    wall.shapeColor=color(255,0,0);
+  }
+  if(damage<10){
+    wall.shapeColor=color(0,255,0);
+  }
+  if(wall.x-bullet.x<(bullet.width+wall.width)/2)
   {
-    car.velocityX=0;
+    bullet.velocityX=0;
     var deformation=0.5*wieght*speed*speed/22509
     if(deformation>180)
     {
-      car.shapeColor=color(225,0,0)
+      bullet.shapeColor=color(225,0,0)
     }
     if(deformation<180 && deformation>100)
     {
-      car.shapeColor=color(230,230,0)
+      bullet.shapeColor=color(230,230,0)
     }
     if(deformation<100);
     {
-      car.shapeColor=color(0,255,0)
+      bullet.shapeColor=color(0,255,0)
     }
   }
-  drawSprites();
 }
+  drawSprites();
+  function hasCollided()
+  {
+    bulletRightEdge=lbullet.x+lbullet.width;
+    wallleftEdge=lwall.x;
+    if(bulletRightEdge>=wallleftEdge){
+      return true
+
+    }
+    return false;
+  }
